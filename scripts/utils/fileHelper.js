177bb9d10b6923f688cc0a6036554e26ee4d3f67
MagSynchro <moraff@gmail.com>
2026-06-30 02:06:44 +0000

    transitioning to PERN organization, updated fileHelper.js, prep for phase 2.

--- scripts/utils/fileHelper.js
+++ scripts/utils/fileHelper.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const JSON_DATA_DIR = path.join(__dirname, '..','jsonfetchfiles');
+const JSON_DATA_DIR = path.join(__dirname, '..','..', 'data');
 
 // Ensure the directory exists
 if (!fs.existsSync(JSON_DATA_DIR)) {
